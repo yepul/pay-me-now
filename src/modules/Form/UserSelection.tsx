3a9b509fe8bffb152e5d61195aae1ded6550b19e
saiful <azwanzin95@gmail.com>
2021-12-22 17:18:18 +0800

    feat: user selection layout, logic

--- src/modules/Form/UserSelection.tsx
+++ src/modules/Form/UserSelection.tsx
@@ -1,9 +1,10 @@
 import { useStoreActions, useStoreState } from "../../store/hooks";
-import { ChangeEvent, FunctionComponent, useMemo } from "react";
 import { useFormik } from "formik";
 import { schema } from "./schema";
+import { ChangeEvent, FunctionComponent, useMemo } from "react";
 import { matchSorter } from "match-sorter";
 import { useDebounce } from "use-debounce";
+import { IUser } from "../../store/model/user";
 
 interface IUserSelection {
   users: string[];
@@ -23,8 +24,8 @@ export const UserSelection: FunctionComponent<IUserSelection> = (props) => {
     validationSchema: schema,
   });
 
-  const handleAddUser = async () => {
-    await formik.handleSubmit();
+  const handleAddUser = () => {
+    formik.handleSubmit();
   };
 
   const [userInput] = useDebounce(formik.values.name, 250);
@@ -34,8 +35,14 @@ export const UserSelection: FunctionComponent<IUserSelection> = (props) => {
       return users;
     }
 
-    return matchSorter(users, formik.values.name, { keys: ["name"] });
+    return matchSorter(users, userInput, { keys: ["name"] });
   }, [users, userInput]);
+
+  const selectedUsers = useMemo<IUser[]>(
+    //@ts-ignore
+    () => props.users.map((userId) => users.find((user) => user.id === userId)),
+    [users, props.users]
+  );
 
   return (
     <div className="mb-4">
@@ -45,15 +52,48 @@ export const UserSelection: FunctionComponent<IUserSelection> = (props) => {
       >
         Participant
       </label>
-      <input
-        className="w-full py-2 pl-3 pr-10 text-left bg-white rounded-lg shadow-md cursor-default focus:outline-none focus-visible:ring-2 focus-visible:ring-opacity-75 focus-visible:ring-white focus-visible:ring-offset-orange-300 focus-visible:ring-offset-2 focus-visible:border-indigo-500 sm:text-sm"
-        id="name"
-        type="text"
-        placeholder="Participant"
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        value={formik.values.name}
-      />
+      <div className="w-full flex py-2 pl-3 pr-10 text-left bg-white rounded-lg shadow-md cursor-default focus:outline-none focus-visible:ring-2 focus-visible:ring-opacity-75 focus-visible:ring-white focus-visible:ring-offset-orange-300 focus-visible:ring-offset-2 focus-visible:border-indigo-500 sm:text-sm">
+        {selectedUsers.map((user) => (
+          <label
+            className="flex align-middle text-gray-700 text-sm mr-1"
+            key={user.id}
+          >
+            {user.name}{" "}
+            <input
+              type="checkbox"
+              id={user.id}
+              value={user.id}
+              className="w-0 h-0"
+              checked={props.users.includes(user.id)}
+              //@ts-ignore
+              onChange={props.handleUser}
+            />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="flex h-4 w-4"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+          </label>
+        ))}
+        <input
+          className="w-full text-left cursor-default focus:outline-none"
+          id="name"
+          type="text"
+          placeholder="Participant"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.name}
+        />
+      </div>
       {formik.errors.name && (
         <span className="text-sm text-red-400">{formik.errors.name}</span>
       )}
@@ -66,11 +106,11 @@ export const UserSelection: FunctionComponent<IUserSelection> = (props) => {
               value={user.id}
               className="w-4 h-4"
               checked={props.users.includes(user.id)}
-              // @ts-ignore
-              onClick={props.handleUser}
+              //@ts-ignore
+              onChange={props.handleUser}
             />
             <label
-              htmlFor="vehicle1"
+              htmlFor={user.id}
               className="text-gray-700 text-sm font-bold mb-1"
             >
               {user.name}
