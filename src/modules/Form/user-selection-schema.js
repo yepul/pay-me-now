bb7546049c64bd394c986b68270238d251ec2034
saiful <azwanzin95@gmail.com>
2021-12-23 16:17:20 +0800

    fix: refactor rename user selection schema

--- src/modules/Form/user-selection-schema.js
+++ src/modules/Form/user-selection-schema.js
@@ -1,6 +1,6 @@
 import * as yup from "yup";
 
-export const schema = yup.object().shape({
+export const userSelectionSchema = yup.object().shape({
   name: yup
     .string()
     .min(3, "name must be more than 3 characters")
