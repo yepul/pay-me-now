e8f000d611c3e2ef082c7ab85943b402cc06b8d4
saiful <azwanzin95@gmail.com>
2021-12-23 15:45:12 +0800

    feat: route to expense form

---
+++ pages/group/[id]/expense.tsx
@@ -0,0 +1,8 @@
+import { FunctionComponent } from "react";
+import { Container } from "../../../src/components/Container";
+
+const Expense: FunctionComponent = () => {
+  return <Container>hello</Container>;
+};
+
+export default Expense;
