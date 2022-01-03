70705489cebc950562401571b7044401cc3a5e6a
saiful <azwanzin95@gmail.com>
2022-01-03 12:20:30 +0800

    feat: modal layout

--- src/components/AutoComplete/index.tsx
+++ src/components/AutoComplete/index.tsx
@@ -1,16 +1,37 @@
 import { FunctionComponent } from "react";
 import { ITextField, TextField } from "../TextField";
 
-interface IAutoComplete extends ITextField {}
+interface ISelection {
+  label: string;
+  value: number;
+}
+
+interface IAutoComplete extends ITextField {
+  selections: ISelection[];
+}
 
 export const AutoComplete: FunctionComponent<IAutoComplete> = ({
   label,
   errors,
+  selections,
   ...textFieldProps
 }) => {
   return (
     <div>
       <TextField label={label} errors={errors} {...textFieldProps} />
+
+      <div id="modal" className="border-black border-2 ">
+        {selections.map(({ value, label }) => (
+          <label key={value} className="flex items-center py-2 pl-3">
+            <input
+              type="checkbox"
+              className="w-4 h-4 align-middle mx-1 border-2"
+              value={label}
+            />
+            {label}
+          </label>
+        ))}
+      </div>
     </div>
   );
 };
