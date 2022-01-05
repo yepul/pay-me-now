b88ac9d5fcf467fbdd1f74afebdefd0a4506885f
saiful <azwanzin95@gmail.com>
2022-01-05 17:13:33 +0800

    feat: refactor TextField component

--- src/components/TextField.tsx
+++ src/components/TextField.tsx
@@ -11,26 +11,24 @@ export const TextField: FunctionComponent<ITextField> = ({
   errors,
   inputAdornment,
   ...inputProps
-}) => {
-  return (
-    <section className="mb-4">
-      <label
-        className="block text-gray-700 text-sm font-bold mb-1"
-        htmlFor="total"
-      >
-        {label}
-      </label>
-      <div className="w-full py-2 flex flex-row px-3 rounded-lg shadow-md">
-        {inputAdornment && (
-          <span className="flex sm:text-sm mr-2">{inputAdornment}</span>
-        )}
-        <input
-          className="w-full flex flex-1 cursor-default focus:outline-none focus-visible:ring-2
+}) => (
+  <div className="mb-4">
+    <label
+      className="block text-gray-700 text-sm font-bold mb-1"
+      htmlFor="total"
+    >
+      {label}
+    </label>
+    <div className="w-full py-2 flex flex-row px-3 rounded-lg shadow-md">
+      {inputAdornment && (
+        <span className="flex sm:text-sm mr-2">{inputAdornment}</span>
+      )}
+      <input
+        className="w-full flex flex-1 cursor-default focus:outline-none focus-visible:ring-2
           focus-visible:ring-opacity-75 focus-visible:ring-white sm:text-sm"
-          {...inputProps}
-        />
-        {errors && <span className="text-sm text-red-400">{errors}</span>}
-      </div>
-    </section>
-  );
-};
+        {...inputProps}
+      />
+      {errors && <span className="text-sm text-red-400">{errors}</span>}
+    </div>
+  </div>
+);
