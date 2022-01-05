ceb211ae049f627b069dd915ec2b41c33b488e32
saiful <azwanzin95@gmail.com>
2022-01-05 17:29:44 +0800

    feat: refactor expense store

--- src/store/model/expense.ts
+++ src/store/model/expense.ts
@@ -1,12 +1,20 @@
-import { Action, action, persist, thunk, Thunk } from "easy-peasy";
+import { Action, action, persist, Thunk, thunk } from "easy-peasy";
 import { IStoreModel } from "../index";
+
+interface IPaymentLogs {
+  user: string;
+  amount: number;
+  created: string; // ISO string date
+}
 
 export interface IExpenseValue {
   id?: string;
   name: string;
-  users: string[];
+  users: string[]; // user.id
   total: number;
   groupId: string;
+  created: string; // ISO string date
+  payment: IPaymentLogs[];
 }
 
 export interface IGetExpensesByGroup extends Omit<IExpenseValue, "groupId"> {}
@@ -15,9 +23,12 @@ export interface IExpense {
   [groupId: string]: IGetExpensesByGroup[];
 }
 
+export interface ISetExpenses
+  extends Omit<IExpenseValue, "created" | "payment"> {}
+
 export interface IExpenseModel {
   expenses: IExpense;
-  setExpenses: Action<IExpenseModel, IExpenseValue>;
+  setExpenses: Action<IExpenseModel, ISetExpenses>;
   getExpenseByGroup: Thunk<
     IExpenseModel,
     string,
@@ -32,7 +43,17 @@ export const expenseModel: IExpenseModel = persist(
     expenses: {},
     setExpenses: action((state, { groupId, ...expense }) => {
       expense["id"] = `expense-${new Date().getTime()}`;
-      state.expenses[groupId].push(expense);
+
+      // initialize new expenses group
+      if (!state.expenses[groupId]) {
+        state.expenses[groupId] = [];
+      }
+
+      state.expenses[groupId].push({
+        ...expense,
+        created: new Date().toISOString(),
+        payment: [],
+      });
     }),
     getExpenseByGroup: thunk((actions, groupId, helpers) => {
       const expenseByGroup = helpers.getState().expenses?.[groupId];
@@ -40,6 +61,7 @@ export const expenseModel: IExpenseModel = persist(
       if (!expenseByGroup) {
         return [];
       }
+
       return expenseByGroup;
     }),
   },
