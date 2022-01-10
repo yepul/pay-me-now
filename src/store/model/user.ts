aff2501fd91f9defab89a7eb49afb18e230b5ec8
saiful <azwanzin95@gmail.com>
2022-01-10 17:02:01 +0800

    feat: computed user by id

--- src/store/model/user.ts
+++ src/store/model/user.ts
@@ -1,5 +1,14 @@
-import { Action, action, persist, thunk, Thunk } from "easy-peasy";
+import {
+  Action,
+  action,
+  computed,
+  Computed,
+  persist,
+  thunk,
+  Thunk,
+} from "easy-peasy";
 import { IStoreModel } from "../index";
+import { IGroup } from "./group";
 
 export interface IUser {
   id: string;
@@ -10,11 +19,16 @@ export interface IUserModel {
   users: IUser[];
   setUsers: Action<IUserModel, string>;
   getUser: Thunk<IUserModel, string, undefined, IStoreModel, IUser>;
+  userById: Computed<IUserModel, (id: string) => IUser>;
 }
 
 export const userModel: IUserModel = persist(
   {
     users: [],
+    userById: computed(
+      (state) => (id) =>
+        state.users.find((user) => user.id === id) ?? { id: "", name: "" }
+    ),
     setUsers: action((state, name) => {
       state.users.push({ name, id: `user-${new Date().getTime()}` });
     }),
