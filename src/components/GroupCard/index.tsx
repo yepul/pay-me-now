3c7ff917f8047e5a7aad80476035c314829b865f
saiful <azwanzin95@gmail.com>
2022-01-13 15:02:51 +0800

    feat: motion GroupCard

--- src/components/GroupCard/index.tsx
+++ src/components/GroupCard/index.tsx
@@ -1,19 +1,52 @@
 import { useStoreState } from "../../store/hooks";
 import dynamic from "next/dynamic";
 import { IGroup } from "../../store/model/group";
+import { motion } from "framer-motion";
 
 const Summary = dynamic<IGroup>(() =>
   import("./Summary").then((mod) => mod.Summary)
 );
 
+const container = {
+  visible: {
+    transition: {
+      when: "beforeChildren",
+      staggerChildren: 0.05,
+    },
+    hidden: {
+      transition: {
+        when: "afterChildren",
+      },
+    },
+  },
+};
+
+const card = {
+  visible: {
+    x: 0,
+    opacity: 1,
+  },
+  hidden: {
+    x: -32,
+    opacity: 0.4,
+  },
+};
+
 export const GroupCard = () => {
   const groups = useStoreState((state) => state.group.groups);
 
   return (
-    <div className="mt-12 grid lg:grid-cols-3 md:grid-cols-2 xs:grid-cols-1 gap-4">
-      {groups.map((group) => (
-        <Summary {...group} key={group.id} />
+    <motion.div
+      variants={container}
+      initial="hidden"
+      animate="visible"
+      className="mt-12 grid lg:grid-cols-3 md:grid-cols-2 xs:grid-cols-1 gap-4"
+    >
+      {groups.map((group, index) => (
+        <motion.div key={group.id} variants={card}>
+          <Summary {...group} />
+        </motion.div>
       ))}
-    </div>
+    </motion.div>
   );
 };
